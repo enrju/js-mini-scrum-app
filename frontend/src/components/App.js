491af54f49a1e1aba0f42a8da84d6a1496f9759b
enrju <enrju@enrju.pl>
2021-07-14 11:37:46 +0200

    feat(front): add database simulation by global variables

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+let projects = [
+    {id: 1, title: "project 1", description: "project 1"}
+];
+
+let tasks = [
+    {id: 1, id_project: 1, id_sprint: null, title: "task 1", where_is: "BACKLOG", minutes: 0},
+    {id: 2, id_project: 1, id_sprint: 1, title: "task 2", where_is: "TODO", minutes: 0},
+];
+
+let sprints = [
+    {id: 1, id_project: 1, title: "sprint - week 1"}
+];
+
 class App extends React.Component {
     state = {
 
