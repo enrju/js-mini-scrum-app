59b514671f6a34ad826bc26a7d76429108a6b4a0
enrju <enrju@enrju.pl>
2021-07-14 17:13:47 +0200

    feat(front): add component ProjectListItem

---
+++ frontend/src/components/ProjectListItem.js
@@ -0,0 +1,26 @@
+import Button from "./Button";
+
+function ProjectListItem (props) {
+    return (    
+        <li>
+            <h2>{props.item.title}</h2>
+            <p>
+                {props.item.isHide ? null : props.item.description}
+            </p>
+            <Button
+                click={props.handleEditProject}
+                description={"edit"}
+            />
+            <Button
+                click={props.handleDeleteProject}
+                description={"delete"}
+            />
+            <Button
+                click={props.handleHideShowProject}
+                description={"hide/show"}
+            />
+        </li> 
+    )
+}
+
+export default ProjectListItem;
