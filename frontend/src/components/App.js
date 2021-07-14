d81f8d3e36fda0211e8a687376fb4427875da036
enrju <enrju@enrju.pl>
2021-07-14 10:52:06 +0200

    feat(front): clean default React application and catalog public

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+            <>
+                <p>DZIAŁA !!!</p>
+            </>
+        )
+    }
+}
+
+export default App;
