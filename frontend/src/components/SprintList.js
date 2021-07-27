5d69fffb122644b1202766db6f0de786fc899574
enrju <enrju@enrju.pl>
2021-07-27 14:42:58 +0200

    feat(front): add component SprintList

---
+++ frontend/src/components/SprintList.js
@@ -0,0 +1,30 @@
+import SprintListItem from "./SprintListItem";
+
+function SprintList (props) {
+    return (
+        <ul>
+            {props.sprintList.map((item) => {
+                const thisSprintTaskList = props.taskList.filter((task) => {
+                    if(task.id_sprint === item.id) return true;
+                    else return false;
+                })
+                return(
+                    <SprintListItem
+                        key={item.id}
+                        item={item}
+                        taskList={thisSprintTaskList}
+                        handleEditSprint={props.handleEditSprint}
+                        handleDeleteSprint={props.handleDeleteSprint}
+                        handleHideShowSprintDetails={props.handleHideShowSprintDetails}
+                        handleEditTask={props.handleEditTask}
+                        handleDeleteTask={props.handleDeleteTask}
+                        handleMoveLeftTask={props.handleMoveLeftTask}
+                        handleMoveRightTask={props.handleMoveRightTask}
+                    />
+                )
+            })}
+        </ul>
+    )
+}
+
+export default SprintList;
