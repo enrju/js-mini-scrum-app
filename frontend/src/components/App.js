b387ba419774dcadf2c678c3d28b9c32b652491e
enrju <enrju@enrju.pl>
2021-08-12 14:46:52 +0200

    feat(front): add method setShowFormEditSprint()

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -196,6 +196,14 @@ class App extends React.Component {
         });
     }
 
+    setShowFormEditSprint(bool) {
+        this.setState(() => {
+            return ({
+                isShowFormEditSprint: bool
+            })
+        });
+    }
+
     getDataFromForm(){
         const inpTitle = document.querySelector("input[name='title']");
         const inpDescription = document.querySelector("textarea[name='description']");
