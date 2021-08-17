4ff71cacb3e4733695796aaf68616b326cc24f33
enrju <enrju@enrju.pl>
2021-08-17 14:10:45 +0200

    feat(back): add server and service static catalog (read all latest frontend)

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const port = process.env.PORT || 3000;
+const host = '127.0.0.1';
+
+const express = require('express');
+const path = require('path');
+
+const server = express();
+server.listen(port, host, () => {
+    console.log('server is working');
+});
+
+server.use(express.static(path.join(__dirname, '../frontend/build')));
+
+//routing here
