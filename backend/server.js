99d807f8d8606d77704172adde0353b8fe01ea6f
enrju <enrju@enrju.pl>
2021-08-19 13:17:16 +0200

    feat(back): update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ const host = '127.0.0.1';
 
 const express = require('express');
 const path = require('path');
-// const db_tmp = require('./my_modules/db_tmp');
-const {projectsRoutes} = require('./routes/projects');
+
+const { projectsRoutes } = require('./routes/projects');
+const { sprintsRoutes } = require('./routes/sprints');
+const { tasksRoutes } = require('./routes/tasks');
 
 const server = express();
 server.listen(port, host, () => {
@@ -15,3 +17,5 @@ server.use(express.static(path.join(__dirname, '../frontend/build')));
 
 //routing here
 projectsRoutes(server);
+sprintsRoutes(server);
+tasksRoutes(server);
