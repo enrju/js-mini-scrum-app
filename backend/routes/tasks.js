12172ba3fc2f101c239d59376b36f4239b069026
enrju <enrju@enrju.pl>
2021-09-24 18:26:30 +0200

    feat(back): update routing for (PUT) /api/tasks/:id

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -52,11 +52,11 @@ module.exports = {
             req.on('data', chunk => body += chunk);
             req.on('end', () => {
                 let obj = JSON.parse(body);
-                
-                db_tmp.db_updateTask(id, obj);
 
-                res.set({'Access-Control-Allow-Origin': '*'});
-                res.send();
+                db_mysql.db_updateTask(id, obj, () => {
+                    res.set({'Access-Control-Allow-Origin': '*'});
+                    res.send();
+                });
             });
         });
 
