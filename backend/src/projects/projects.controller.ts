f8681eafd09c7d856c775cd9444700447caf0c1e
enrju <enrju@enrju.pl>
2022-09-28 17:45:41 +0200

    (be): add temporary router [GET] /api/v2/projects to check DB connection

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { pool } from "../utils/db";
 
-@Controller('projects')
-export class ProjectsController {}
+@Controller('/api/v2/projects')
+export class ProjectsController {
+  @Get('/')
+  async getAll() {
+    const [results] = await pool.execute('SELECT * FROM `projects`');
+    return results;
+  }
+}
