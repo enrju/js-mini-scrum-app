83b6e85e9e48b0f7fb5b6f69de99da216615b8ac
enrju <enrju@enrju.pl>
2022-09-30 17:01:23 +0200

    (be): update record ProjectRecord - add method update()

--- backend/src/projects/records/project.record.ts
+++ backend/src/projects/records/project.record.ts
@@ -4,6 +4,7 @@ import { FieldPacket, ResultSetHeader } from "mysql2";
 
 type ProjectRecordResults = [ProjectRecord[], FieldPacket[]];
 type ProjectRecordInsertResult = ResultSetHeader[];
+type ProjectRecordUpdateResult = ResultSetHeader[];
 
 export class ProjectRecord implements ProjectEntity {
   id: number;
@@ -17,13 +18,13 @@ export class ProjectRecord implements ProjectEntity {
   }
 
   static async getAll(): Promise<ProjectRecord[]> {
-    const [results] = (await pool.execute('SELECT * FROM `projects`')) as ProjectRecordResults;
+    const [results] = (await pool.execute("SELECT * FROM `projects`")) as ProjectRecordResults;
 
     return results.map(item => new ProjectRecord(item));
   }
 
   static async getOne(id: number): Promise<ProjectRecord | null> {
-    const [results] = (await pool.execute('SELECT * FROM `projects` WHERE `id` = :id', {
+    const [results] = (await pool.execute("SELECT * FROM `projects` WHERE `id` = :id", {
       id,
     })) as ProjectRecordResults;
 
@@ -31,12 +32,25 @@ export class ProjectRecord implements ProjectEntity {
   }
 
   async insert(): Promise<number> {
-    const result = (await pool.execute('INSERT INTO `projects` VALUES(:id, :title, :description)', {
+    const result = (await pool.execute("INSERT INTO `projects` VALUES(:id, :title, :description)", {
       id: 'NULL',
       title: this.title,
       description: this.description,
     })) as ProjectRecordInsertResult;
 
     return result[0].insertId;
+  }
+
+  async update(title: string, description: string | null = null): Promise<number> {
+    const result = (await pool.execute(
+      "UPDATE `projects` " +
+      "SET `title` = :title, `description` = :description " +
+      "WHERE `id` = :id", {
+        id: this.id,
+        title: title,
+        description: description,
+      })) as ProjectRecordUpdateResult;
+
+    return result[0].changedRows;
   }
 }
