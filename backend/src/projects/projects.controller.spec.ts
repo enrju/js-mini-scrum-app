635317c13db2a839e8caf4e322bba6442a6f2876
enrju <enrju@enrju.pl>
2022-10-02 16:10:50 +0200

    (be): test controller - add MOCK

--- backend/src/projects/projects.controller.spec.ts
+++ backend/src/projects/projects.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from "./projects.service";
+import { ProjectRecord } from "./records/project.record";
+import { pool } from "../utils/db";
 
 describe('ProjectsController', () => {
   let controller: ProjectsController;
@@ -16,5 +18,19 @@ describe('ProjectsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  let testProjectRecord: ProjectRecord;
+  let testProjectRecordInsertedId: number;
+
+  beforeAll(() => {
+    testProjectRecord = new ProjectRecord({
+      title: 'test',
+      description: 'testowy',
+    });
+  });
+
+  afterAll(async () => {
+    await pool.end();
   });
 });
