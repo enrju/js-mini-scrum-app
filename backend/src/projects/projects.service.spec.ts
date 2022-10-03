4852e4dfd4164b6fc8751afe0bb353abc5bf19e7
enrju <enrju@enrju.pl>
2022-10-03 11:14:04 +0200

    (be): test service.getOne() - throw RecordNotFoundError

--- backend/src/projects/projects.service.spec.ts
+++ backend/src/projects/projects.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsService } from './projects.service';
+import { RecordNotFoundError } from "../utils/errors";
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
@@ -14,5 +15,23 @@ describe('ProjectsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  test('getOne(id) for not existed id should throw RecordNotFoundError', async () => {
+    const resGetAll = await service.getAll();
+
+    if(resGetAll.isSuccess) {
+      let maxId = 0;
+
+      resGetAll.data.forEach(item => {
+        if(item.id > maxId) maxId = item.id;
+      });
+
+      try {
+        await service.getOne(maxId + 1);
+      } catch (e) {
+        expect(e).toBeInstanceOf(RecordNotFoundError);
+      }
+    }
   });
 });
