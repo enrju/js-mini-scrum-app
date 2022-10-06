4c7db1397fefc6292505b5a073d2ab8abd50714a
enrju <enrju@enrju.pl>
2022-10-06 10:35:29 +0200

    (be): update SprintsService - add getAllForProject()

--- backend/src/sprints/sprints.service.ts
+++ backend/src/sprints/sprints.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { GetAllSprintsForProjectResponse, Sprint } from "../types";
+import { SprintRecord } from "./records/sprint.record";
 
 @Injectable()
-export class SprintsService {}
+export class SprintsService {
+  async getAllForProject(id: string): Promise<GetAllSprintsForProjectResponse> {
+    const result = await SprintRecord.getAllForProject(Number(id));
+
+    return {
+      isSuccess: true,
+      data: result as Sprint[],
+    }
+  }
+}
