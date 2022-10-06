153958d7ac144b327501fd2d53b5becdd137cb50
enrju <enrju@enrju.pl>
2022-10-06 10:40:31 +0200

    (be): inject SprintsService into ProjectsController

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Inject, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, forwardRef, Get, Inject, Param, Post, Put } from "@nestjs/common";
 import {
   GetAllProjectsResponse,
   GetOneProjectResponse,
@@ -9,32 +9,34 @@ import {
 import { ProjectsService } from "./projects.service";
 import { CreateProjectDto } from "./dto/create-project.dto";
 import { UpdateProjectDto } from "./dto/update-project.dto";
-import { RecordValidationError } from "../utils/errors";
+import { GetAllSprintsForProjectResponse } from "../types/sprint/sprint.response";
+import { SprintsService } from "../sprints/sprints.service";
 
 @Controller('/api/v2/projects')
 export class ProjectsController {
   constructor(
-    @Inject(ProjectsService) private projectService: ProjectsService,
+    @Inject(ProjectsService) private projectsService: ProjectsService,
+    @Inject(forwardRef(() => SprintsService)) private sprintsService: SprintsService,
   ) {
   }
 
   @Get('/')
   async getAll(): Promise<GetAllProjectsResponse> {
-    return this.projectService.getAll();
+    return this.projectsService.getAll();
   }
 
   @Get('/:id')
   async getOne(
     @Param('id') id: string,
   ): Promise<GetOneProjectResponse> {
-    return this.projectService.getOne(id);
+    return this.projectsService.getOne(id);
   }
 
   @Post('/')
   async insert(
     @Body() createProjectDto: CreateProjectDto,
   ): Promise<CreateProjectResponse> {
-    return this.projectService.insert(createProjectDto);
+    return this.projectsService.insert(createProjectDto);
   }
 
   @Put('/:id')
@@ -42,13 +44,13 @@ export class ProjectsController {
     @Param('id') id: string,
     @Body() updateProjectDto: UpdateProjectDto,
   ): Promise<UpdateProjectResponse> {
-    return this.projectService.update(id, updateProjectDto);
+    return this.projectsService.update(id, updateProjectDto);
   }
 
   @Delete('/:id')
   async delete(
     @Param('id') id: string,
   ): Promise<DeleteProjectResponse> {
-    return this.projectService.delete(id);
+    return this.projectsService.delete(id);
   }
 }
