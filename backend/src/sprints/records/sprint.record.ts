89f698cf605cb0b92120321d20d785064a20569a
enrju <enrju@enrju.pl>
2022-10-06 10:31:09 +0200

    (be): update SprintRecord - add getAllForProject()

--- backend/src/sprints/records/sprint.record.ts
+++ backend/src/sprints/records/sprint.record.ts
@@ -1,14 +1,25 @@
 import { SprintEntity } from "../entities/sprint.entity";
+import { pool } from "../../utils/db";
+import { FieldPacket } from "mysql2";
+
+type SprintRecordResults = [SprintRecord[], FieldPacket[]];
 
 export class SprintRecord implements SprintEntity {
   id: number;
-  projectId: number;
   title: string;
+  project_id: number;
 
   constructor(obj: SprintEntity) {
     this.id = obj.id;
     this.title = obj.title;
-    this.projectId = obj.projectId;
+    this.project_id = obj.project_id;
   }
 
+  static async getAllForProject(id: number): Promise<SprintRecord[]> {
+    const [results] = (await pool.execute("SELECT * FROM `sprints` WHERE `project_id` = :id", {
+      id,
+    })) as SprintRecordResults;
+
+    return results.map(item => new SprintRecord(item));
+  }
 }
