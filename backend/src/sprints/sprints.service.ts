dc994fe36e36a1895c2fac2e65765dd304dab1ce
enrju <enrju@enrju.pl>
2022-10-06 13:31:30 +0200

    (be): update SprintsService - add validateId()

--- backend/src/sprints/sprints.service.ts
+++ backend/src/sprints/sprints.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { GetAllSprintsForProjectResponse, Sprint } from "../types";
 import { SprintRecord } from "./records/sprint.record";
+import { RecordValidationError } from "../utils/errors";
 
 @Injectable()
 export class SprintsService {
+  validateId(id: string) {
+    if(!Number(id)
+      || !Number.isInteger(Number(id))) {
+      throw new RecordValidationError('Id in not an integer number');
+    }
+  }
+
   async getAllForProject(id: string): Promise<GetAllSprintsForProjectResponse> {
+    this.validateId(id);
+
     const result = await SprintRecord.getAllForProject(Number(id));
 
     return {
