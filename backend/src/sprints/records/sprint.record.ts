5a59e030dadcd3ac3531854c0c0fff35924175ae
enrju <enrju@enrju.pl>
2022-10-06 13:30:12 +0200

    (be): update SprintRecord - add validate()

--- backend/src/sprints/records/sprint.record.ts
+++ backend/src/sprints/records/sprint.record.ts
@@ -1,6 +1,8 @@
 import { SprintEntity } from "../entities/sprint.entity";
 import { pool } from "../../utils/db";
 import { FieldPacket } from "mysql2";
+import { sprintConfig } from "../../types";
+import { RecordValidationError } from "../../utils/errors";
 
 type SprintRecordResults = [SprintRecord[], FieldPacket[]];
 
@@ -10,9 +12,19 @@ export class SprintRecord implements SprintEntity {
   project_id: number;
 
   constructor(obj: SprintEntity) {
+    this.validate(obj);
+
     this.id = obj.id;
     this.title = obj.title;
     this.project_id = obj.project_id;
+  }
+
+  validate(obj: SprintEntity): void {
+    if(!obj.title
+      || obj.title.length < sprintConfig.title.min
+      || obj.title.length > sprintConfig.title.max) {
+      throw new RecordValidationError(`Title must have from ${sprintConfig.title.min} to ${sprintConfig.title.max} signs`);
+    }
   }
 
   static async getAllForProject(id: number): Promise<SprintRecord[]> {
