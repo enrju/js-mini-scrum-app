9ecab519d438546f31ca5da87c5c951c834afa84
enrju <enrju@enrju.pl>
2022-10-10 12:54:45 +0200

    (be): test SprintsController - add inject SprintsService

--- backend/src/sprints/sprints.controller.spec.ts
+++ backend/src/sprints/sprints.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SprintsController } from './sprints.controller';
+import { SprintsService } from "./sprints.service";
 
 describe('SprintsController', () => {
   let controller: SprintsController;
@@ -7,6 +8,7 @@ describe('SprintsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SprintsController],
+      providers: [SprintsService],
     }).compile();
 
     controller = module.get<SprintsController>(SprintsController);
