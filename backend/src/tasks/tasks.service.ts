05ddb54b355d6fba5b7a9d9fa45326801abf2f05
enrju <enrju@enrju.pl>
2022-10-13 10:03:56 +0200

    (be): update TasksService - add getAllForProject()

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { GetAllTasksForProjectResponse, Task } from "../types";
+import { TaskRecord } from "./records/task.record";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  async getAllForProject(id: string): Promise<GetAllTasksForProjectResponse> {
+    //projectId was validated in ProjectService
+
+    const result = await TaskRecord.getAllForProject(Number(id));
+
+    return {
+      isSuccess: true,
+      data: result as Task[],
+    }
+  }
+}
