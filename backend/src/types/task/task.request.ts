e71db6297307a6a5e55c9761421ea9a2176672a0
enrju <enrju@enrju.pl>
2022-10-17 10:28:35 +0200

    (be): add type UpdateTaskRequest

--- backend/src/types/task/task.request.ts
+++ backend/src/types/task/task.request.ts
@@ -1,3 +1,5 @@
 import { Task } from "./task.type";
 
 export type CreateTaskForProjectRequest = Omit<Task, 'id' | 'state' | 'minutes' | 'project_id' | 'sprint_id'>;
+
+export type UpdateTaskRequest = Omit<Task, 'id' | 'state' | 'minutes' | 'project_id' | 'sprint_id'>;
