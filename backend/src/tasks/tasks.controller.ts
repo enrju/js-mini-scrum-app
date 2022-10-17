8c72b6295a94f1b62325b667e1e9271d453b14c8
enrju <enrju@enrju.pl>
2022-10-17 10:49:27 +0200

    (be): update TasksController - add router [PUT] /api/v2/tasks/:id

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Inject } from "@nestjs/common";
+import { Body, Controller, Inject, Param, Put } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
+import { UpdateSprintDto } from "../sprints/dto/update-sprint.dto";
+import { UpdateSprintResponse, UpdateTaskResponse } from "../types";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
 @Controller('/api/v2/tasks')
 export class TasksController {
   constructor(
     @Inject(TasksService) private tasksService: TasksService,
   ) {
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<UpdateTaskResponse> {
+    return this.tasksService.update(id, updateTaskDto);
   }
 }
