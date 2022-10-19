67a69ff7c061bb946f812610ada06110156762b3
enrju <enrju@enrju.pl>
2022-10-19 12:10:19 +0200

    (be): update settings in taskConfig in /types

--- backend/src/types/task/task.config.ts
+++ backend/src/types/task/task.config.ts
@@ -1,5 +1,9 @@
 import { TaskState } from "./task.type";
 
+const ONE_MINUTE_IN_MS = 1000 * 60; //1 minute = 1000ms * 60;
+const DELTA_TIME_IN_MIN = 1;
+const DELTA_TIME_IN_MS = ONE_MINUTE_IN_MS * DELTA_TIME_IN_MIN;
+
 export const taskConfig = {
   id: {min: 0},
   title: {min: 3, max: 256},
@@ -8,4 +12,6 @@ export const taskConfig = {
   minutes: {min: 0},
   project_id: {min: 0},
   sprint_id: {min: 0},
+  DELTA_TIME_IN_MIN: DELTA_TIME_IN_MIN,
+  DELTA_TIME_IN_MS: DELTA_TIME_IN_MS,
 }
