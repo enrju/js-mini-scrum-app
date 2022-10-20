2a41ab626f3b0966b80a4813a564393925ad87cb
enrju <enrju@enrju.pl>
2022-10-20 13:26:23 +0200

    (be): update SprintsModule - complete inject TasksService

--- backend/src/sprints/sprints.module.ts
+++ backend/src/sprints/sprints.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from "@nestjs/common";
 import { SprintsController } from './sprints.controller';
 import { SprintsService } from './sprints.service';
+import { TasksModule } from "../tasks/tasks.module";
 
 @Module({
+  imports: [
+    forwardRef(() => TasksModule),
+  ],
   controllers: [SprintsController],
   providers: [SprintsService],
   exports: [SprintsService],
