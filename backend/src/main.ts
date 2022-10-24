e4ec8fbf358655f0f52d742b1b8bd5e162e5c437
enrju <enrju@enrju.pl>
2022-10-24 11:23:34 +0200

    (be): configure cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {appConfig} from "../config/app-config";
 import { GlobalExceptionFilter } from "./filters/global-exception.filter";
+import { NestExpressApplication } from "@nestjs/platform-express";
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  (app as NestExpressApplication).use(cors({
+    origin: appConfig.corsOrigin,
+  }));
 
   app.useGlobalFilters(new GlobalExceptionFilter());
 
