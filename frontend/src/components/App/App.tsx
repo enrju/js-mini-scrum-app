f23223211c69f27fba1cb8d4434e08b4a896a4ce
enrju <enrju@enrju.pl>
2022-10-25 10:40:10 +0200

    (fe): add interface AppData

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
-import {TaskState} from 'types';
+import {Project, Sprint, Task} from 'types';
 
-function App() {
-  //test working shared types
-  const tmp: TaskState = TaskState.BACKLOG;
-
-  return (
-    <div className="App">
-      <h1>Test - cra app</h1>
-    </div>
-  );
+interface AppData {
+    idOpenedProject: number | null;
+    titleOpenedProject: string;
+    descriptionOpenedProject: string;
+    taskListOpenedProject: Task[];
+    sprintListOpenedProject: Sprint[];
+    isBacklogHide: boolean;
+    idChosenSprint: number | null;
+    editedTaskIndex: number;
+    isShowFormAddTask: boolean;
+    isShowFormEditTask: boolean;
+    editedSprintIndex: number;
+    isShowFormAddSprint: boolean;
+    isShowFormEditSprint: boolean;
+    projectList: Project[];
+    editedProjectIndex: number;
+    isShowFormAddProject: boolean;
+    isShowFormEditProject: boolean;
 }
 
-export default App;
+export const App = () => {
+
+    return (
+        <h1>Test - cra app</h1>
+    );
+}
